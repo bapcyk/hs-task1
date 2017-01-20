59970c75f10b4af70db444fa83b5720ffa0c2cfa
pavel <yosifov.pv@gmail.com>
2017-01-20 08:38:58 +0200

    Added WordLoc record

--- src/Lib.hs
+++ src/Lib.hs
@@ -3,6 +3,13 @@ module Lib
     ) where
 
 import System.Process (readProcess)
+
+-- | Location of found word
+data WordLoc = WordLoc {
+  file  :: String  -- file where word was found
+, nline :: Integer -- line number where word was found
+  }
+
 -- diff :: IO ()
 diff rev0 rev1 gitbin = do
   readProcess gitbin ["diff", rev0 ++ ".." ++ rev1] [] >>= putStrLn
