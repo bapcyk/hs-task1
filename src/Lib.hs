ed3be35e0efd97e64b80d0b265ad01c50e2337b2
pavel <yosifov.pv@gmail.com>
2017-01-21 09:51:40 +0200

    Added parse of no-new-line-at-end-of-file

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,4 @@
-module Lib
-    ( gitdiff
-    ) where
+module Lib where
 
 import System.Process (readProcess)
 
@@ -9,7 +7,7 @@ import System.Process (readProcess)
 data DiffFiles = DiffFiles {
   fileA :: String  -- 1st file path
 , fileB :: String  -- 2nd file path
-  }
+  } deriving Show
 
 -- | Hunk lines marker, looks like:
 -- "@@ -7,11 +7,14 @@ ..."
@@ -18,24 +16,39 @@ data DiffRange = DiffRange {
 , lenA    :: Integer -- file A hunk effected lines number
 , begB    :: Integer -- fileB hunk start line
 , lenB    :: Integer -- file B hunk effected lines number
-  }
+  } deriving Show
 
 -- | Kind of line change: it was added/deleted/unmodified
 data ChangeAction = Add | Del | No
+  deriving Show
 
 -- | Marker of line, looks like:
--- TODO add support of newline
+-- "+ ..." or "- ..." or "..."
 data DiffLine = DiffLine {
   change  :: ChangeAction -- kind of change
 , line    :: String       -- line itself
-  }
+  } deriving Show
+
+data DiffNL = DiffNL
+  deriving Show
 
 -- | Found diff marker
 data Marker =
-  NLMarker                -- new-line-at-end-of-file marker
+  NLMarker DiffNL         -- new-line-at-end-of-file marker
   | LineMarker DiffLine   -- changed/unchanged line marker
   | RangeMarker DiffRange -- range marker
   | FilesMarker DiffFiles -- compared files marker
+  deriving Show
+
+instance Read DiffNL where
+  readsPrec _ "\\No new line at end of file" = [(DiffNL, "")]
+  readsPrec _ _ = []
+
+-- identMarker :: String -> Marker
+-- identMarker s =
+--   if s == "\\ No newline at end of file" then
+--     NLMarker
+--     else if "@@"
 
 -- | Get diff between revision `rev0`..`rev1` of local master branch
 gitdiff :: String -> String -> String -> IO String
