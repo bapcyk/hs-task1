bf46a6d12e0df1f50863964160d2f32204576660
pavel <yosifov.pv@gmail.com>
2017-01-21 19:03:43 +0200

    Renamed parse, added comments

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,5 +1,6 @@
 module Lib where
 
+
 import Data.Char
 import Control.Monad (guard)
 import Control.Monad.Trans.State
@@ -9,12 +10,14 @@ import Text.Read (readMaybe)
 import System.Process (readProcess)
 import Data.List (isPrefixOf)
 
+
 -- | Hunk compared files marker, looks like:
 -- "diff --git a/app/Main.hs b/app/Main.hs"
 data DiffFiles = DiffFiles {
   fileA :: String  -- 1st file path
 , fileB :: String  -- 2nd file path
   } deriving Show
+
 
 -- | Hunk lines marker, looks like:
 -- "@@ -7,11 +7,14 @@ ..."
@@ -25,9 +28,11 @@ data DiffRange = DiffRange {
 , lenB    :: Integer -- file B hunk effected lines number
   } deriving Show
 
+
 -- | Kind of line change: it was added/deleted/unmodified
 data ChangeAction = Add | Del | No
   deriving Show
+
 
 -- | Marker of line, looks like:
 -- "+ ..." or "- ..." or "..."
@@ -36,8 +41,10 @@ data DiffLine = DiffLine {
 , line    :: String       -- line itself
   } deriving Show
 
+
 data DiffNL = DiffNL
   deriving Show
+
 
 -- | Found diff marker
 data Marker =
@@ -47,13 +54,9 @@ data Marker =
   | FilesMarker DiffFiles -- compared files marker
   deriving Show
 
--- | Drop `this` sequence from sequence `s` and returns Just tail, but if `s` is
--- not prefixed by `this` then returns Nothing
--- dropThis :: Eq a => [a] -> [a] -> Maybe [a]
--- dropThis this s =
---   if this `isPrefixOf` s then Just $ drop (length this) s
---   else Nothing
 
+-- | "Reads" some items (chars) until `end` returns True. Returns readed items
+-- and tail (not readed) because it uses state
 readRawUntil :: (a -> Bool) -> State [a] [a]
 readRawUntil end = do
   s <- get
@@ -61,16 +64,16 @@ readRawUntil end = do
   put $ drop (length taken) s
   return taken
 
-maybeStr a b = if a == b then Just a else Nothing
 
 -- | Reader of DiffNL diff's string
 instance Read DiffNL where
   readsPrec _ "\\No new line at end of file" = [(DiffNL, "")]
   readsPrec _ _ = []
 
+
 -- | Parses DiffRange string, returns (range::Maybe DiffRange, notParsed::String)
-parse :: MaybeT (State String) DiffRange
-parse = do
+parseDiffRange :: MaybeT (State String) DiffRange
+parseDiffRange = do
   -- Line:
   --   lift (readRawUntil isNumber) >>= guard . ("@@ -"==)
   -- is the same as:
@@ -88,15 +91,13 @@ parse = do
   lenB <- lift (readRawUntil isSpace)
   return $ DiffRange (int begA) (int lenA) (int begB) (int lenB)
 
+
 instance Read DiffRange where
   readsPrec _ s =
-    case runState (runMaybeT parse) s of
+    case runState (runMaybeT parseDiffRange) s of
       (Nothing, _) -> []
       (Just range, _) -> [(range, "")]
-  -- readsPrec _ s = readRawUntil
-  -- readsPrec _ s
-  --   | "@@ " `isPrefixOf` s = [(DiffRange 0 0 0 0, "")]
-  --   | otherwise = []
+
 
 -- | Get diff between revision `rev0`..`rev1` of local master branch
 gitdiff :: String -> String -> String -> IO String
